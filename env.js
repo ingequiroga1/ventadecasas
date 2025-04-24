fad1feac09e695eff46677d53eb37f62242a410a
ingequiroga1 <ingequiroga1@gmail.com>
2025-04-24 14:57:21 -0600

    Se agrega env

---
+++ env.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://uhxlmskvuxholdkghqeb.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoeGxtc2t2dXhob2xka2docWViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzMjQxMzMsImV4cCI6MjA0NTkwMDEzM30.xM0rzOByXYpyGAWI16FyzWeeWbBZApoQBiT85yuwkKE';
+
+export {SUPABASE_URL,SUPABASE_ANON_KEY};
+
